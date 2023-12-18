3c707e945de446e54eb6fa05a3640933ce68be67
Stefan Oderbolz <oderbolz@gmail.com>
2023-12-18 10:29:23 +0100

    Refactor crawl_url to be less complex

--- lib/website_matcher.py
+++ lib/website_matcher.py
@@ -95,55 +95,7 @@ def match_texts(texts, keywords, old_hashes):
     return matches
 
 
-def crawl_urls(
-    url, label, group, timeout, level, dl_type, keywords, old_hashes, verify=True
-):
-    if level >= 2 or not url:
-        log.debug(f"Level: {level}, URL: {url}, skipping..")
-        return
-
-    global all_urls
-    if url in all_urls:
-        log.debug(f"URL '{url}' already crawled. Skipping...")
-        return
-    all_urls.append(url)
-
-    log.info(f"Crawl from URL {url}")
-    try:
-        content_type = dl.get_content_type(url, verify=verify)
-        if "application/pdf" in content_type:
-            content = dl.pdfdownload(url)
-            split_text = content.split("\n\n")
-            matches = match_texts(split_text, keywords, old_hashes)
-            log.debug(f"Matches: {matches}")
-
-            if matches:
-                yield {
-                    "type": "PDF",
-                    "group": group,
-                    "url": url,
-                    "label": label,
-                    "matches": matches,
-                }
-            return
-        elif "text" not in content_type:
-            raise ValueError(
-                f"Unsupported content type: {content_type}, skipping URL {url}"
-            )
-        if dl_type == "static":
-            content = dl.download_content(url, verify=verify)
-        elif dl_type == "dynamic":
-            content = dl.download_with_selenium(url, timeout)
-        else:
-            raise Exception(f"Invalid type: {dl_type}")
-    except (RequestException, WebDriverException, ValueError):
-        log.exception(f"Error when trying to request from URL: {url}")
-        if level == 0:
-            raise
-        return
-
-    soup = BeautifulSoup(content, "html.parser")
-
+def match_html(soup, keywords, old_hashes):
     matches = []
     for kw_re in keywords:
         log.info(f"Check keyword: {kw_re['keyword']}")
@@ -185,14 +137,79 @@ def crawl_urls(
                     "hashes": source_hashes,
                 }
             )
+    return matches
+
+
+def get_content(url, verify, dl_type, timeout):
+    log.info(f"Get content from URL {url}")
+    try:
+        content_type = dl.get_content_type(url, verify=verify)
+        if "application/pdf" in content_type:
+            content = dl.pdfdownload(url)
+        elif "text" not in content_type:
+            raise ValueError(
+                f"Unsupported content type: {content_type}, skipping URL {url}"
+            )
+
+        if dl_type == "static":
+            content = dl.download_content(url, verify=verify)
+        elif dl_type == "dynamic":
+            content = dl.download_with_selenium(url, timeout)
+        else:
+            raise Exception(f"Invalid type: {dl_type}")
+        return (content_type, content)
+    except (RequestException, WebDriverException, ValueError):
+        log.exception(f"Error when trying to request from URL: {url}")
+        raise ValueError(f"Error when trying to request from URL: {url}")
+
+
+def get_title(soup, label):
+    try:
+        return soup.title.string.strip() or label
+    except AttributeError:
+        return label
+
+
+def crawl_urls(
+    url, label, group, timeout, level, dl_type, keywords, old_hashes, verify=True
+):
+    if level >= 2 or not url:
+        log.debug(f"Level: {level}, URL: {url}, skipping..")
+        return
+
+    global all_urls
+    if url in all_urls:
+        log.debug(f"URL '{url}' already crawled. Skipping...")
+        return
+    all_urls.append(url)
+
+    try:
+        content_type, content = get_content(url, verify, dl_type, timeout, level)
+    except ValueError:
+        if level == 0:
+            raise
+        # if a sub-page returns an error, silently ignore it
+        return
+
+    if "application/pdf" in content_type:
+        split_text = content.split("\n\n")
+        matches = match_texts(split_text, keywords, old_hashes)
+        log.debug(f"Matches: {matches}")
+
+        source_type = "PDF"
+        title = label
+
+        # create empty soup for PDFs
+        soup = BeautifulSoup("", "html.parser")
+    else:
+        soup = BeautifulSoup(content, "html.parser")
+        matches = match_html(soup, keywords, old_hashes)
+        source_type = "HTML"
+        title = get_title(soup, label)
 
     if matches:
-        try:
-            title = soup.title.string.strip() or label
-        except AttributeError:
-            title = label
         yield {
-            "type": "HTML",
+            "type": source_type,
             "group": group,
             "url": url,
             "label": title,
@@ -201,15 +218,11 @@ def crawl_urls(
 
     # find all links with href attribute, limit to 500 results
     for link in soup.find_all("a", href=True, limit=500):
-        try:
-            # skip empty or anchor links
-            href = link["href"]
-            if not href or href.startswith("#") or href.startswith("mailto:"):
-                continue
-            absolute_url = urllib.parse.urljoin(url, href)
-        except KeyError:
-            log.debug(f"No href in link: {link}")
+        href = link["href"]
+        # skip empty or anchor links
+        if not href or href.startswith("#") or href.startswith("mailto:"):
             continue
+        absolute_url = urllib.parse.urljoin(url, href)
 
         link_text = link.string or label
         yield from crawl_urls(
